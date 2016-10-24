256eef36d0843451a1d61cfea1085d695f972706
Oscar Barlow <oscar.barlow@gmail.com>
2016-10-24 15:07:14 +0100

    put code snippets into methods instead

--- directory.rb
+++ directory.rb
@@ -14,10 +14,21 @@ students = [
   "Norman Bates",
 ]
 # and the print them
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-  puts student
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
-# finally we print the total
-puts "Overall, we have #{students.size} great students"
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
