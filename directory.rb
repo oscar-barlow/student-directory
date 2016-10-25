df8aa13cafd683410dd257aef4297b243aa0e6fc
Oscar Barlow <oscar.barlow@gmail.com>
2016-10-25 11:41:19 +0100

    Print student deets if name begins with D and name length is less than 12

--- directory.rb
+++ directory.rb
@@ -40,7 +40,7 @@ end
 
 def print(students)
   students.each_with_index do |student, index|
-      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)" if "#{student[:name]}"[0] == "D"
+      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)" if ("#{student[:name]}"[0] == "D") && (student[:name].length < 12)
   end
 end
 
