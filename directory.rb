3752ba5696341ad8ac3757c4f94396080f2e0de7
Oscar Barlow <oscar.barlow@gmail.com>
2016-10-26 17:10:14 +0100

    add methods for writing students to file

--- directory.rb
+++ directory.rb
@@ -72,32 +72,33 @@ end
 
 # ------------------------ #
 #                          #
-# Interactive Menu Methods #
+#     Interactive Menu     #
 #                          #
 # ------------------------ #
 
 def interactive_menu
   loop do
     print_menu
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "9"
-      exit # this will cause the program to terminate
-    else
-      puts "I don't know what you meant, try again"
-    end
+    process(gets.chomp)
   end
 end
 
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
+end
+
+def process(selection)
+  case selection
+    when "1" then input_students
+    when "2" then show_students
+    when "3" then save_students
+    when "9" then exit
+    else
+      "I don't know what you mean?"
+    end
 end
 
 def show_students
@@ -108,3 +109,21 @@ end
 
 # nothing happens til we call the methods
 interactive_menu
+
+# ------------------------ #
+#                          #
+#  Save @students to file  #
+#                          #
+# ------------------------ #
+
+def save_students
+  # open file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
